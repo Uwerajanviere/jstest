50448c1a23e416eb03e4d362089d9b8094b0a6ef
uwera janviere <janviereuwera43@gmail.com>
2023-09-03 11:47:29 -0700

    added new file

---
+++ test.js
@@ -0,0 +1,25 @@
+document.write("<h1>hello dolly</h1>")
+
+function message(){
+    alert("this alert box was called with the onload event and you are welcome");
+}
+{
+
+    document.write("<h2>this is aheading</h2>");
+    document.write("<p>this is a paragraph</p>");
+    document.write("<p>this is a nother paragraph </p>");
+}
+{
+
+    document.write("<h2>this is aheading</h2>");
+    document.write("<p>this is a paragraph</p>");
+    document.write("<p>this is a nother paragraph </p>");
+}
+
+var tex1="my name is";
+var text2="uwera janviere";
+
+document.write(text1);
+
+
+
